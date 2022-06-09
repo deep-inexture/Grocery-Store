f4172e411cba15166706ff3e5950ebef4c6649ed
Deep <deep.inexture@gmail.com>
2022-06-09 16:00:54 +0530

    Changed Database URL from Local to Heroku-Postgres

--- grocerystore/routers/authentication.py
+++ grocerystore/routers/authentication.py
@@ -26,6 +26,8 @@ def registration(request: schemas.User, db: Session= Depends(get_db)):
 @router.post('/login')
 def login(request: OAuth2PasswordRequestForm= Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
+    if request.username != "admin@admin.in":
+        raise HTTPException(status_code=401, detail="UnAuthorized User! Only Admin Access Allowed.")
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
     if not Hash.verify(user.password, request.password):
