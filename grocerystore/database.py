fcde6d8201f7912f4a326f153e7912cfc0d9dcb8
Deep <deep.inexture@gmail.com>
2022-06-09 15:52:05 +0530

    Merged code from feature branch

--- grocerystore/database.py
+++ grocerystore/database.py
@@ -6,8 +6,8 @@ import os
 
 load_dotenv()
 
-# engine = sqlalchemy.create_engine(os.environ.get('DATABASE_URL'), echo=True)
-engine = create_engine(os.environ.get('LOCAL_DATABASE_URL'), echo=True)
+engine = sqlalchemy.create_engine(os.environ.get('DATABASE_URL'), echo=True)
+# engine = create_engine(os.environ.get('LOCAL_DATABASE_URL'), echo=True)
 
 Base = declarative_base()
 
