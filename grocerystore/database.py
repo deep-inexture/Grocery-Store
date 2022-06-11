e12a4db58dcc9cf8df94db078558f36dc7e19fc3
Deep <deep.inexture@gmail.com>
2022-06-11 10:55:28 +0530

    Updated Database Config

--- grocerystore/database.py
+++ grocerystore/database.py
@@ -8,8 +8,8 @@ import os
 load_dotenv()
 
 # Connecting to Database using env variable which stores database URL.
-# engine = sqlalchemy.create_engine(os.environ.get('DATABASE_URL_POSTGRES'), echo=True)
-engine = create_engine(os.environ.get('LOCAL_DATABASE_URL'), echo=True)
+engine = sqlalchemy.create_engine(os.environ.get('DATABASE_URL_POSTGRES'), echo=True)
+# engine = create_engine(os.environ.get('LOCAL_DATABASE_URL'), echo=True)
 
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine)
