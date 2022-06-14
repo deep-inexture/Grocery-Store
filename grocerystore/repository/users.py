c94a93ff932012b90d2a67c9af91a63308197b00
Deep <deep.inexture@gmail.com>
2022-06-14 18:14:35 +0530

    User Search Operations and filters completed

--- grocerystore/repository/users.py
+++ grocerystore/repository/users.py
@@ -1,7 +1,6 @@
-from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from .. import models, schemas
-from typing import List
+from .. import models
+from sqlalchemy import and_
 
 
 def view_products(db: Session):
@@ -10,12 +9,15 @@ def view_products(db: Session):
 
 
 def search_by_name(name: str, db: Session):
-    name_filter = db.query(models.Product).filter(models.Product.title.like(name)).all()
+    name_filter = db.query(models.Product).filter(models.Product.title.like(name+'%')).all()
     return name_filter
 
 
-# def search_by_price(max_price: float, min_price: float, db: Session):
-#     price_filter = db.query(models.Product).where(models.Product.price == max_price)
-#     # print(price_filter)
-#     # print(getattr(price_filter, 'price'))
-#     return price_filter
+def search_by_price(max_price: float, min_price: float, db: Session):
+    price_filter = db.query(models.Product).filter(models.Product.price > min_price, models.Product.price < max_price).all()
+    return price_filter
+
+
+def search_by_name_and_price(name: str, max_price: float, min_price: float, db: Session):
+    name_and_price_filter = db.query(models.Product).filter(and_(and_(models.Product.price > min_price, models.Product.price < max_price), (models.Product.title.like(name+'%')))).all()
+    return name_and_price_filter
