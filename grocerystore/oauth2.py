f844c18fc56bbf07cc97ac253d1f4b34272d3cea
Deep <deep.inexture@gmail.com>
2022-06-22 16:16:22 +0530

    Added Pagination | Orders Payment Segement

--- grocerystore/oauth2.py
+++ grocerystore/oauth2.py
@@ -19,3 +19,16 @@ def get_current_user(data: str = Depends(oauth2_scheme)):
     )
     email_token = token.verify_token(data, credentials_exception)
     return email_token
+
+
+def get_current_user_access_token(data: str = Depends(oauth2_scheme)):
+    """
+    This function provides email-id to function who has access to routes after successful Login.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid ACCESS TOKEN. Please Check and come back!!!",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    email_token = token.verify_refresh_token(data, credentials_exception)
+    return email_token
